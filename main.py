2df4583ddf0d54e2b444b4c4bd38041c287908c2
Omer Gaiznger <Omerg863@gmail.com>
2024-01-08 17:20:42 +0200

    webhook

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, Updat
 from dotenv import load_dotenv
 import pymongo
 import certifi
+import time
 
 load_dotenv()
 
 
 TOKEN = os.getenv('TOKEN')
+COUPONS_URL = os.getenv('COUPONS_URL')
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -258,6 +260,121 @@ async def echo(update, context):
     """Echo the user message."""
     await update.message.reply_text(update.effective_message.text)
 
+
+async def coupon_scrape(url):
+    try:
+        response = requests.get(COUPONS_URL, headers={'User-Agent': 'Mozilla/5.0'}).text
+        soup = BeautifulSoup(response, "html.parser")
+        list_of_coupons = soup.find("div", {"class": "eq_grid pt5 rh-flex-eq-height col_wrap_three"})
+        articles = list_of_coupons.find_all("article")
+        first_name = articles[0].find("h3", {"class": "flowhidden mb10 fontnormal position-relative"})
+        first_coupon_url = first_name.find("a")["href"]
+        second_name = articles[1].find("h3", {"class": "flowhidden mb10 fontnormal position-relative"})
+        second_coupon_url = second_name.find("a")["href"]
+        urls2 = [first_coupon_url, second_coupon_url]
+        new_coupons, urls = connect_to_db_coupons(urls2, True)
+        print(urls2)
+        if new_coupons:
+            courses = []
+            hit = False
+            index = 0
+            for article in articles:
+                try:
+                    name = article.find("h3", {"class": "flowhidden mb10 fontnormal position-relative"})
+                    coupon_url = name.find("a")["href"]
+                    if index != 0:
+                        if coupon_url in urls:
+                            hit = True
+                            break
+                    else:
+                        if coupon_url == urls[0]:
+                            hit = True
+                            break
+                    percent = article.find("span", {"class": "grid_onsale"}).text
+                    if "100%" not in percent:
+                        continue
+                    image = article.find("img", {"class": "ezlazyload"})["data-ezsrc"]
+                    time.sleep(4)
+                    courses.append({"name": name.text, "url": coupon_url, "image": image, "percent": percent})
+                except Exception as e:
+                    print(e)
+                    print("False coupon found")
+                index += 1
+            if index < 11:
+                for course in courses:
+                    await send_coupons(course["name"], course["percent"], course["url"], course["image"])
+            else:
+                await send_coupons_list(courses)
+            return [new_coupons, hit, urls2]
+    except Exception as e:
+        print(e)
+        return False
+    return [new_coupons]
+
+async def get_coupons():
+    ##"""Get the coupons from the website."""
+    print("Checking coupons...")
+    try:
+        out = await coupon_scrape(COUPONS_URL)
+        if out[0]:
+            if not out[1]:
+                print("page 2")
+                await coupon_scrape(COUPONS_URL + 'page/2/')
+            connect_to_db_coupons(out[2], False)
+    except Exception as e:
+        print(e)
+
+def connect_to_db_coupons(urls, read):
+    ca = certifi.where()
+    client = pymongo.MongoClient(os.environ.get("MONGODB_ACCESS"), tlsCAFile=ca)
+    db = client.new_database
+    if not read:
+        query = {"_id" : 1 }
+        db.coupons.replace_one(query ,{"url": urls[0], "url2": urls[1], "_id" : 1})
+    else:
+        settings = db.coupons.find_one({"_id": 1})
+        urls2 = [settings["url"], settings["url2"]]
+        if urls[0] == urls2[0] or urls[1] == urls2[1]:
+            print("No new coupons found")
+            return [False, urls2]
+        return [True, urls2]
+
+async def send_coupons(name, percent, coupon_url, image):
+    ca = certifi.where()
+    client = pymongo.MongoClient(os.environ.get("MONGODB_ACCESS"), tlsCAFile=ca)
+    db = client.new_database
+    chat_ids = db.registered.find()
+    for chat_id in chat_ids:
+        is_waiting = db.waiting.find_one({"_id": chat_id['_id']})
+        if is_waiting != None:
+            db.gathered.insert_one({"chat_id": chat_id['_id'], "name": name, "coupon_url": coupon_url, "image": image, "percent": percent})
+            print("Added to waiting list")
+        else:
+            await application.bot.sendPhoto(chat_id=chat_id["_id"], photo=image, caption=f'{name} is {percent}: {coupon_url}')
+            print("sent coupon")
+
+async def send_coupons_list(coupons):
+    ca = certifi.where()
+    client = pymongo.MongoClient(os.environ.get("MONGODB_ACCESS"), tlsCAFile=ca)
+    db = client.new_database
+    chat_ids = db.registered.find()
+    message = ""
+    index = 1
+    for coupon in coupons:
+        name = coupon["name"]
+        percent = coupon["percent"]
+        coupon_url = coupon["url"]
+        message += f"{index}. {name} is {percent}: {coupon_url}\n"
+    for chat_id in chat_ids:
+        is_waiting = db.waiting.find_one({"_id": chat_id['_id']})
+        if is_waiting != None:
+            for coupon in coupons:
+                db.gathered.insert_one({"chat_id": chat_id['_id'], "name": coupon['name'], "coupon_url": coupon['coupon_url'], "image": coupon['image'], "percent": coupon['percent']})
+                print("Added to waiting list")
+        else:
+            await application.bot.sendMessage(chat_id=chat_id["_id"], text=message)
+            print("sent coupons list")
+
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TOKEN).build()
 
@@ -291,4 +408,4 @@ if __name__ == '__main__':
 
 
     
-    application.run_polling()
+    application.run_webhook("0.0.0.0", port=80,url_path="https://telegram-notifications-bot.onrender.com", allowed_updates=Update.ALL_TYPES)
