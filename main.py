c913f1bbbd50076d6164b9c153a00a5ec9699f8c
Omer Gaiznger <Omerg863@gmail.com>
2024-01-08 13:29:28 +0200

    start

--- main.py
+++ main.py
@@ -1,78 +1,26 @@
-# main.py
-from bs4 import BeautifulSoup
-import requests
 import logging
 import os
-import signal
-import pymongo
-import certifi
-import time
-from PyPDF2 import PdfFileReader
-import io
-from contextlib import asynccontextmanager
-from http import HTTPStatus
 from telegram import Update
-from telegram.ext import Application, CommandHandler
-from telegram.ext._contexttypes import ContextTypes
-from fastapi import FastAPI, Request, Response
-from selenium import webdriver
-from selenium.common.exceptions import TimeoutException, WebDriverException
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.proxy import Proxy, ProxyType
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver import ActionChains
-
-from webdriver_manager.chrome import ChromeDriverManager
-
-from random_user_agent.user_agent import UserAgent
-from random_user_agent.params import SoftwareName, OperatingSystem
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
-DEBUG = os.environ.get("DEBUG_VALUE") == "True"
-PORT = int(os.environ.get('PORT', 80))
-TOKEN = os.environ.get("NOTIFICATIONS_BOT_TOKEN")
-WEBHOOK_URL = os.environ.get("WEBHOOK_URL")
+TOKEN = os.getenv('TOKEN')
 
-coupons_url = os.environ.get("COUPONS_URL")
-
-
-# Initialize python telegram bot
-ptb = (
-    Application.builder()
-    .updater(None)
-    .token(TOKEN) # replace <your-bot-token>
-    .read_timeout(7)
-    .get_updates_read_timeout(42)
-    .build()
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
 )
 
-@asynccontextmanager
-async def lifespan(_: FastAPI):
-    await ptb.bot.setWebhook(WEBHOOK_URL) # replace <your-webhook-url>
-    async with ptb:
-        await ptb.start()
-        yield
-        await ptb.stop()
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
-# Initialize FastAPI app (similar to Flask)
-app = FastAPI(lifespan=lifespan)
-
-@app.post("/")
-async def process_update(request: Request):
-    req = await request.json()
-    update = Update.de_json(req, ptb.bot)
-    await ptb.process_update(update)
-    return Response(status_code=HTTPStatus.OK)
-
-# Example handler
-async def start(update, _: ContextTypes.DEFAULT_TYPE):
-    """Send a message when the command /start is issued."""
-    await update.message.reply_text("starting...")
-
-ptb.add_handler(CommandHandler("start", start))
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(TOKEN).build()
+    
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+    
+    application.run_polling()
